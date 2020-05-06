2bb8985ed84abbcc6c72691d3ccd89745a40b862
gonzalo.guasch <gonzaloguasch@outlook.com>
2020-05-06 12:03:03 -0300

    me hinche los huevos de css

--- src/Catalogo.js
+++ src/Catalogo.js
@@ -4,9 +4,21 @@ import './Catalogo.css'
 function Catalogo(){
     return(
         <div>
-            CATALOGO
+         <div class="flip-card">
+           <div class="flip-card-inner">
+              <div class="flip-card-front">
+                <img src="http://cdn3.upsocl.com/wp-content/uploads/2019/05/Johnny-Bravo-hVKMu1juz-b.jpg" alt="Avatar" width="300px;" height="300px;"/>
+               </div>
+            <div class="flip-card-back">
+                <p>Hola soy jhonny bravo</p>
+            </div>
         </div>
-    )
+        </div> 
+        </div>)
 }
 
 export default Catalogo
+
+
+
+
